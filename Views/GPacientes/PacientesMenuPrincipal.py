b951501c719330529d6f33c6a1030237d665aa9d
David_Cuadros <daviddelaluz18@gmail.com>
2025-12-01 14:41:06 -0600

    Guardando antes de main

--- Views/GPacientes/PacientesMenuPrincipal.py
+++ Views/GPacientes/PacientesMenuPrincipal.py
@@ -65,7 +65,7 @@ def PacientesMenuPrincipal(root_principal=None):
 
     tk.Label(
         root,
-        text="Sistema de gestión de pacientes",
+        text="Elige la accion deseas realizar:",
         bg=PALETA["fondo"],
         fg="#1e4e5a",
         font=("Arial", 13)
